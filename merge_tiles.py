672316fa09514b7bbccf0d31738a598cdf16877f
Enrico Ciraci <eciraci@uci.edu>
2024-02-27 11:27:38 +0100

    merge_tiles.py - Added - Merge 2D Deformation data relative to a single AOI into a single file.

--- merge_tiles.py
+++ merge_tiles.py
@@ -1,14 +1,13 @@
-#!/usr/bin/env python3
 """
 Written by Enrico Ciraci' - February 2024
-Merge burst data relative to a single AOI into a single file.
+Merge 2D Deformation data relative to a single AOI into a single file.
 
-usage: merge_burst.py [-h] [-D OUT_DIR] index_file
+usage: merge_tiles.py [-h] [-D OUT_DIR] index_file
 
-Merge GSP Bursts belonging to track into a single Product.
+Merge GSP 2D Deformation Tiles (East-West and Vertical) into a single Product.
 
 positional arguments:
-  index_file            Index file containing the list of bursts available
+  index_file            Index file containing the list ofTiles available
                         over the AOI.
 
 options:
@@ -29,6 +28,7 @@ Python Dependencise:
     - typing: Type hints
     - lxml: XML validation against a schema
     - pathlib: Work with file paths
+
 """
 # - Python modules
 import os
@@ -58,14 +58,14 @@ SENSOR = 'SNT'    # - Sentinel-1
 def main() -> None:
     # - Parse command line arguments
     parser = argparse.ArgumentParser(
-        description="Merge GSP Bursts belonging to track into "
-                    "a single Product.",
+        description="Merge GSP 2D Deformation Tiles (East-West and Vertical) "
+                    "into a single Product.",
         formatter_class=argparse.ArgumentDefaultsHelpFormatter
     )
     # - burst_file: Sentinel-1 burst index file file
     parser.add_argument('index_file', type=str,
                         help='Index file containing the list of'
-                             'bursts available over the AOI.')
+                             'Tiles available over the AOI.')
 
     # - Optional arguments
     # - Output directory
@@ -88,6 +88,12 @@ def main() -> None:
     # - Load the index file
     logging.info(f"# - Loading index file {args.index_file}")
     gdf = gpd.read_file(args.index_file)
+    # - Remove lines with Path set to None
+    gdf = gdf[gdf['Path'] != 'None']
+    if gdf.empty:
+        logging.error(f"# - No Tiles found in the index "
+                      f"file {args.index_file}")
+        return
 
     # - Extract AOI info
     mask_name = args.index_file.split('\\')[-1].replace('.shp', '')
@@ -96,70 +102,62 @@ def main() -> None:
     out_dir = os.path.join(args.out_dir, str(aoi_info['aoi_tag']))
     os.makedirs(out_dir, exist_ok=True)
 
-    # - Get the list of tracks
-    tracks = gdf['Track'].unique()
-
-    # - Loop over the tracks
-    for track in tracks:
-        # - Filter the GeoDataFrame by track
-        track_gdf = gdf[gdf['Track'] == track]
-        logging.info(f"# - Processing track {track} with "
-                     f"{len(track_gdf)} bursts")
-
-        # - Select all the lines with Path value different from None
-        track_gdf = track_gdf[track_gdf['Path'] != 'None']
-        # - verify if there are bursts to merge
-        if len(track_gdf) == 0:
-            logging.warning(f"# - No bursts found for track {track}")
+    # - Merge Tiles based on the Ortho Values - East-West and Vertical
+    # - i.e., Deformation Direction
+    logging.info(f"# - Merging Tiles based on Deformation Direction [V, E]")
+    for ortho in ['V', 'E']:
+        # - Filter Tiles based on the Ortho Value
+        gdf_ortho = gdf[gdf['Ortho'] == ortho]
+        if gdf_ortho.empty:
+            logging.error(f"# - No Tiles found for Ortho {ortho}")
             continue
 
-        # - Load the first burst
-        fb_path = Path(track_gdf.iloc[0]['Path'])
-        gdf_brst = read_as_geodataframe(fb_path)
+        # - Merge Tiles
+        # - Load the first tile
+        ft_path = Path(gdf_ortho.iloc[0]['Path'])
+        gdf_tiles = read_as_geodataframe(ft_path)
 
         # - Extract Metadata from the first burst archive
-        xml_dicts_ref = extract_xml_from_zip(str(fb_path))[0]
+        xml_dicts_ref = extract_xml_from_zip(str(ft_path))[0]
         # - Extract fields from the metadata
         in_prod_id = xml_dicts_ref['product_id']
-        in_prod_id_ns = xml_dicts_ref['product_id'].replace('-', '')
-        in_start_date = xml_dicts_ref['start_date'].replace('-', '')
-        in_end_date = xml_dicts_ref['end_date'].replace('-', '')
+        in_prod_id_ns = xml_dicts_ref['product_id'].replace('_', '')
+        in_start_date = str(ft_path.stem).split('_')[2]
+        in_end_date = str(ft_path.stem).split('_')[3]
         provider_ref = xml_dicts_ref['provider']
 
         # - Loop over the remaining bursts and merge them
-        for idx in range(1, len(track_gdf)):
-            brst_path = Path(track_gdf.iloc[idx]['Path'])
-            gdf_brst = gdf_brst._append(read_as_geodataframe(brst_path))
+        for idx in range(1, len(gdf_ortho)):
+            tile_path = Path(gdf_ortho.iloc[idx]['Path'])
+            gdf_tiles = gdf_ortho._append(read_as_geodataframe(tile_path))
 
         # - Remove duplicates based on Longitude and Latitude coordinates
-        gdf_brst = gdf_brst.drop_duplicates(subset=['latitude', 'longitude'])
-        print(f"# - Number of bursts merged: {len(gdf_brst)}")
-        print(f"# - Number of unique pid: {len(gdf_brst['pid'].unique())}")
+        gdf_tiles = gdf_tiles.drop_duplicates(subset=['easting', 'northing'])
+        print(f"# - Number of bursts merged: {len(gdf_tiles)}")
 
         # - Output File Name
         out_f_name = (f"ISS_{in_prod_id_ns}_{in_start_date}"
-                      f"_{in_end_date}_{track}"
-                      f"{aoi_info['aoi_tag']}F_01")
+                      f"_{in_end_date}_{aoi_info['aoi_tag']}O{ortho}_01")
         out_file = os.path.join(out_dir, out_f_name + '.csv')
         logging.info(f"# - Saving merged file {out_file}")
-        gdf_brst.to_csv(out_file, index=False)
+        gdf_tiles.to_csv(out_file, index=False)
 
         # - Covert the Detaframe Geometry to EPSG:4326 (WGS84)
-        gdf_brst = gdf_brst.to_crs("EPSG:4326")
+        gdf_tiles = gdf_tiles.to_crs("EPSG:4326")
 
         # - Generate the metadata file
         metadata_f_name = (f"ISS_{in_prod_id_ns}_{in_start_date}"
-                           f"_{in_end_date}_{track}"
-                           f"{aoi_info['aoi_tag']}F_01.xml")
+                           f"_{in_end_date}_{aoi_info['aoi_tag']}"
+                           f"O{ortho}_01.xml")
         metadata_file = os.path.join(out_dir, metadata_f_name)
         logging.info(f"# - Generating metadata file {metadata_file}")
 
         # - Extract Bounding Box and Perimeter
-        x_min, y_min, x_max, y_max = gdf_brst.total_bounds
+        x_min, y_min, x_max, y_max = gdf_tiles.total_bounds
         # - extract dataset envelope polygon
         envelope \
-            = gpd.GeoSeries([gdf_brst.unary_union.envelope],
-                            crs=gdf_brst.crs).iloc[0].exterior.coords.xy
+            = gpd.GeoSeries([gdf_tiles.unary_union.envelope],
+                            crs=gdf_tiles.crs).iloc[0].exterior.coords.xy
         xs = envelope[0]
         ys = envelope[1]
         crd = list(zip(xs, ys))
@@ -184,9 +182,6 @@ def main() -> None:
         sensor = ET.SubElement(root, 'sensor_id')
         sensor.text = SENSOR
 
-        track_id = ET.SubElement(root, 'track_id')
-        track_id.text = track
-
         provider = ET.SubElement(root, 'provider')
         provider.text = provider_ref
 
@@ -196,12 +191,10 @@ def main() -> None:
             = xml_dicts_ref['production_date'].replace('-', '')
 
         start_date = ET.SubElement(root, 'start_date')
-        start_date.text \
-            = xml_dicts_ref['start_date'].replace('-', '')
+        start_date.text = in_start_date
 
         end_date = ET.SubElement(root, 'end_date')
-        end_date.text \
-            = xml_dicts_ref['end_date'].replace('-', '')
+        end_date.text = in_end_date
 
         # - Add AOI
         aoi = ET.SubElement(root, 'aoi')
@@ -226,40 +219,32 @@ def main() -> None:
             add_meta_field(dataset, md)
 
         # - Loop over the remaining bursts and merge them
-        for idx in range(len(track_gdf)):
-            meta_path = Path(track_gdf.iloc[idx]['Path'])
+        for idx in range(len(gdf_ortho)):
+            meta_path = Path(gdf_ortho.iloc[idx]['Path'])
             xml_dict_gsp = extract_xml_from_zip(str(meta_path))[0]
-
-            # - Valid only for TRE-A data
-            # - Extract Input Product type from file name
-            p_f_name = Path(track_gdf.iloc[idx]['Path']).stem.split('_')
-            if p_f_name[4].endswith('B'):
-                # - Single Geometry Deformation
-                gsp_id_in = f'S301{SENSOR}01'
-            else:
-                # - Single Geometry Calibrated Deformation
-                gsp_id_in = f'S301{SENSOR}02'
+            product_id_in = str(meta_path.stem)
 
             # - Extract Geospatial Product ID
             gsp = ET.SubElement(dataset, 'gsp')
             gsp_id = ET.SubElement(gsp, 'gsp_id')
+            # - 2D Deformation East-West and Vertical Components
+            # - Input ID is Fixed in this case
+            gsp_id_in = 'S301SNT03'
             gsp_id.text = gsp_id_in
 
             # - Extract Product ID - input file name
-            product_id_in = Path(track_gdf.iloc[idx]['Path']).stem
+            p_f_name = str(Path(gdf_ortho.iloc[idx]['Path']).stem).split('_')
 
             # - Convert the input file name to the original
             # - one produced by SVC01
-            product_id_in\
-                = product_id_in.replace(p_f_name[1], gsp_id_in)
             product_id_in \
-                = product_id_in.replace(p_f_name[4], p_f_name[4][:-1])
+                = product_id_in.replace(p_f_name[1], gsp_id_in)
 
             product_id = ET.SubElement(gsp, 'product_id')
             product_id.text = product_id_in
 
-            burst_id = ET.SubElement(gsp, 'burst_id')
-            burst_id.text = xml_dict_gsp['burst_id']
+            tile_id = ET.SubElement(gsp, 'tile_id')
+            tile_id.text = xml_dict_gsp['tile_id']
 
             description = ET.SubElement(gsp, 'description')
             description.text = gsp_description(gsp_id_in)
